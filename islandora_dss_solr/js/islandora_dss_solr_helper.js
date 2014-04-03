f430afc9d3f9ab59a40924d34f60386d46a67c92
jrgriffiniii <griffinj@lafayette.edu>
2014-04-03 17:24:48 -0400

    DSSSM-656 #time 1h 4m Implementing JavaScript for the default value within the field

---
+++ islandora_dss_solr/js/islandora_dss_solr_helper.js
@@ -0,0 +1,38 @@
+/**
+ * @file Functionality for basic UI features for the Islandora DSS Solr features   
+ * @author griffinj@lafayette.edu
+ *
+ *
+ */
+
+"use strict";
+
+/**
+ * Drupal integration
+ *
+ */
+(function($, Drupal) {
+
+    Drupal.behaviors.islandoraDssSolr = {
+
+	attach: function(context, settings) {
+
+	    $('.islandora-solr-search-field option').click(function(e) {
+
+		    if($(this).val() == 'dc.date') {
+
+			$(this).parents('.fieldset-wrapper').find('.form-text')
+			.val('Ex. 1925 or 1925-03')
+			.click(function(e) {
+
+				if($(this).val() == 'Ex. 1925 or 1925-03') {
+
+				    $(this).val('');
+				}
+			    });
+		    }
+		});
+	}
+    };
+
+})(jQuery, Drupal);
