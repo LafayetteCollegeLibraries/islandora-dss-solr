0dfaafbb94549356e4eb88f37c755ee1ce3cbcc8
jrgriffiniii <griffinj@lafayette.edu>
2014-04-10 16:57:57 -0400

    DSSSM-656 #time 28m Implementing the example text for all date fields within the Advanced Search widget

--- islandora_dss_solr/js/islandora_dss_solr_helper.js
+++ islandora_dss_solr/js/islandora_dss_solr_helper.js
@@ -17,9 +17,23 @@
 
 	attach: function(context, settings) {
 
+	    var ISLANDORA_DSS_SOLR_DATE_FIELDS = [
+						  'dc.date',
+						  'eastasia.Date.Artifact.Lower',
+						  'eastasia.Date.Artifact.Upper',
+						  'eastasia.Date.Image.Lower',
+						  'eastasia.Date.Image.Upper',
+						  'eastasia.Date.Original',
+						  'geology_slides_esi.date.original',
+						  'mdl_prints.date.original',
+						  'war_casualties.date.birth.search',
+						  'war_casualties.date.death.search',
+						  'mckelvy.date.original.search',
+						  ];
+
 	    $('.islandora-solr-search-field option').click(function(e) {
 
-		    if($(this).val() == 'dc.date') {
+		    if(ISLANDORA_DSS_SOLR_DATE_FIELDS.indexOf( $(this).val() ) !== -1) {
 
 			$(this).parents('.fieldset-wrapper').find('.form-text')
 			.val('Ex. 1925, 1925-03, or 1925-03-01')
